421d2804ebc3361e8a9a2f37134f27c95b36618d
PS-ADAS <65344060+poojasawantADAS@users.noreply.github.com>
2025-12-05 23:04:43 -0500

    Update ImageFrame.cpp

--- src/ImageFrame.cpp
+++ src/ImageFrame.cpp
@@ -1,5 +1,17 @@
-#pragma once
+#include "ImageFrame.h"
+#include <sstream>
 
-class ImageFrame {
+ImageFrame::ImageFrame(int width, int height, double timestamp)
+    : width_(width), height_(height), timestamp_(timestamp),
+      pixels_(width * height, 0) {}
 
-};
+int ImageFrame::getWidth() const { return width_; }
+int ImageFrame::getHeight() const { return height_; }
+double ImageFrame::getTimestamp() const { return timestamp_; }
+
+std::string ImageFrame::getInfo() const {
+    std::ostringstream oss;
+    oss << "ImageFrame: " << width_ << "x" << height_
+        << ", timestamp=" << timestamp_;
+    return oss.str();
+}
