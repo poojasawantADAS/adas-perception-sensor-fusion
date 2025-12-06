2358e9b52b4c63e918bb8ea9c4ce371432ace2af
PS-ADAS <65344060+poojasawantADAS@users.noreply.github.com>
2025-12-05 23:04:02 -0500

    Update ImageFrame.h

--- include/ImageFrame.h
+++ include/ImageFrame.h
@@ -1,1 +1,21 @@
-Header Files
+#pragma once
+#include <vector>
+#include <cstdint>
+#include <string>
+
+class ImageFrame {
+public:
+    ImageFrame(int width, int height, double timestamp);
+
+    int getWidth() const;
+    int getHeight() const;
+    double getTimestamp() const;
+    std::string getInfo() const;
+
+private:
+    int width_;
+    int height_;
+    double timestamp_;
+    std::vector<uint8_t> pixels_;
+};
+
