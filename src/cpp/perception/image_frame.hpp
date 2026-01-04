aebec9f3da83c780c69f38f0efb5047e0410f9c3
Pooja Sawant <poojasawant179@gmail.com>
2026-01-03 21:40:40 -0500

    Baseline: executable builds and runs without OpenCV

--- src/cpp/perception/image_frame.hpp
+++ src/cpp/perception/image_frame.hpp
@@ -1,18 +1,14 @@
 #pragma once
-
-#include <opencv2/opencv.hpp>
 #include <chrono>
 
 class ImageFrame {
 public:
     using TimePoint = std::chrono::steady_clock::time_point;
 
-    ImageFrame(const cv::Mat& img, TimePoint ts);
+    explicit ImageFrame(TimePoint timestamp);
 
-    const cv::Mat& image() const;
     TimePoint timestamp() const;
 
 private:
-    cv::Mat image_;
     TimePoint timestamp_;
 };
